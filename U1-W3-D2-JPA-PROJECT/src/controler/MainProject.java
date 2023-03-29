8b1510451ed071e7b7afb34c5828b1b79cb80084
Adina Gabriela Iamandi <iamandi.adina95@gmail.com>
2023-03-29 15:12:47 +0200

    jpa homework

--- U1-W3-D2-JPA-PROJECT/src/controler/MainProject.java
+++ U1-W3-D2-JPA-PROJECT/src/controler/MainProject.java
@@ -5,6 +5,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.persistence.Query;
+
 import model.User;
 
 public class MainProject {
---
+++ U1-W3-D2/src/model/EventoDAO.java
@@ -0,0 +1,9 @@
+package model;
+
+import javax.persistence.EntityManager;
+
+public class EventoDAO {
+	
+	static EntityManager em;
+em.getEmf().createEntityManager();
+}
