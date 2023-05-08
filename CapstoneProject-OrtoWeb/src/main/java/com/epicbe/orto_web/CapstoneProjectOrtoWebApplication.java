c8fc608c668086ee2e5b38631d9240b29976f27d
Adina Gabriela Iamandi <iamandi.adina95@gmail.com>
2023-05-08 12:26:39 +0200

    .

---
+++ CapstoneProject-OrtoWeb/src/main/java/com/epicbe/orto_web/CapstoneProjectOrtoWebApplication.java
@@ -0,0 +1,13 @@
+package com.epicbe.orto_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneProjectOrtoWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoneProjectOrtoWebApplication.class, args);
+	}
+
+}
---
+++ CapstoneProject-OrtoWeb/src/test/java/com/epicbe/orto_web/CapstoneProjectOrtoWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epicbe.orto_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneProjectOrtoWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
